356185083023e3ca8fcd7994a1f78e5bdbc6d7c2
parthasarathireddy <pardhu0548@gmail.com>
2017-10-03 20:01:37 +0530

    Screen 2 - list created

--- app/src/main/java/com/bestbuy/dal/bestbuy/MainActivity.java
+++ app/src/main/java/com/bestbuy/dal/bestbuy/MainActivity.java
@@ -2,12 +2,28 @@ package com.bestbuy.dal.bestbuy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button viewProducts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewProducts = (Button) findViewById(R.id.getStarted);
+        viewProducts.setOnClickListener(this);
+    }
+    String[] values = new String[]{"Android", "iPhone", "WindowsMobile",
+            "Blackberry", "WebOS", "Ubuntu"};
+
+    @Override
+    public void onClick(View view) {
+        Intent MainIntent = new Intent(MainActivity.this, ProductActivity.class);
+        MainIntent.putExtra("stringCollection", values);
+        startActivity(MainIntent);
     }
 }
--- app/src/main/java/com/bestbuy/dal/bestbuy/ProductActivity.java
+++ app/src/main/java/com/bestbuy/dal/bestbuy/ProductActivity.java
@@ -1,13 +1,52 @@
 package com.bestbuy.dal.bestbuy;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ProductActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+
+        final ListView ProductList = (ListView) findViewById(R.id.productList);
+
+        Intent intent = getIntent();
+        String[] values = intent.getStringArrayExtra("stringCollection");
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+        final ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        ProductList.setAdapter(adapter);
+
+        ProductList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                String text = (String) ProductList.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(), text + " - " + position, Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
     }
 }
+
+
